04afe9d0567f6482f70e16d20175558dea74f21b
sahil <ersahilgalundia17@gmail.com>
2021-03-30 16:48:24 +0530

    adding dbs

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://admin-sahil:test123@cluster0.kq2ve.mongodb.net/blogDB", {useNewUrlParser: true});
 
 const postSchema = {
   title: String,
